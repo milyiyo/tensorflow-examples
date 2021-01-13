65277fdfa8b614bb48e08d3ceda180a98f4a4638
milyiyo <milyiyo@gmail.com>
2021-01-13 20:44:05 +0000

    Remove commented code

--- 01_celsius_to_fahrenheit/one_layer_model.py
+++ 01_celsius_to_fahrenheit/one_layer_model.py
@@ -9,8 +9,6 @@ logger.setLevel(logging.ERROR)
 celsius_q    = np.array([-40, -10,  0,  8, 15, 22,  38],  dtype=float)
 fahrenheit_a = np.array([-40,  14, 32, 46, 59, 72, 100],  dtype=float)
 
-# for i,c in enumerate(celsius_q):
-#   print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
 l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
 
 model = tf.keras.Sequential([l0])
