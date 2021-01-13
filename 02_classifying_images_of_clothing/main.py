9100f683f974a1b48fdc192ec7b67aa6187c72cb
milyiyo <milyiyo@gmail.com>
2021-01-13 21:57:10 +0000

    Add first version of classifying fashion

---
+++ 02_classifying_images_of_clothing/main.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+
+# Import TensorFlow Datasets
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+# Helper libraries
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+class_names = metadata.features['label'].names
+print("Class names: {}".format(class_names))
+
+num_train_examples = metadata.splits['train'].num_examples
+num_test_examples = metadata.splits['test'].num_examples
+print("Number of training examples: {}".format(num_train_examples))
+print("Number of test examples:     {}".format(num_test_examples))
+
+def normalize(images, labels):
+  images = tf.cast(images, tf.float32)
+  images /= 255
+  return images, labels
+
+# The map function applies the normalize function to each element in the train
+# and test datasets
+train_dataset =  train_dataset.map(normalize)
+test_dataset  =  test_dataset.map(normalize)
+
+# The first time you use the dataset, the images will be loaded from disk
+# Caching will keep them in memory, making training faster
+train_dataset =  train_dataset.cache()
+test_dataset  =  test_dataset.cache()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+BATCH_SIZE = 32
+train_dataset = train_dataset.cache().repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
+test_dataset = test_dataset.cache().batch(BATCH_SIZE)
+
+model.fit(train_dataset, epochs=5, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
+
+test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples/32))
+print('Accuracy on test dataset:', test_accuracy)
+
+# Make predictions
+for test_images, test_labels in test_dataset.take(1):
+  test_images = test_images.numpy()
+  test_labels = test_labels.numpy()
+  predictions = model.predict(test_images)
+
+print(np.argmax(predictions[0]))
+
+# Grab an image from the test dataset
+img = test_images[0]
+print(img.shape)
+
+# Add the image to a batch where it's the only member.
+img = np.array([img])
+print(img.shape)
+
+# Predict the image
+predictions_single = model.predict(img)
+print(predictions_single)
+print(np.argmax(predictions_single[0]))
+
+print('End here')
